12850f4103223a53b7b9beacfcefa459a8a79752
apecatikov <antoinepecatikov@gmail.com>
2019-01-08 19:25:03 -0500

    refac: navigation bar partial clean up

--- consultation-site/src/components/layout.js
+++ consultation-site/src/components/layout.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import { Scroller, scrollInitialState } from 'react-skroll'
 
 import Header from './header'
 import './layout.css'
-
-const colors = [
-  { name: 'Blue', color: '#215cf4' },
-  { name: 'Cyan', color: '#0ccabf' },
-  { name: 'Green', color: '#4ac36c' },
-  { name: 'Yellow', color: '#e0be18' },
-  { name: 'Red', color: '#e91e4f' },
-  { name: 'Magenta', color: '#ca28e4' },
-]
 
 class Layout extends React.Component {
   render() {
@@ -32,18 +22,13 @@ class Layout extends React.Component {
         `}
         render={data => (
           <>
-            {/* <Header scroll={scroll} siteTitle={data.site.siteMetadata.title} /> */}
             <div
               style={{
-                margin: `0 auto`,
-                maxWidth: 960,
                 padding: `100px 1.0875rem 1.45rem`,
               }}
             >
               {this.props.children}
-              <footer>
-                © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-              </footer>
+              <footer />
             </div>
           </>
         )}
--- consultation-site/src/pages/index.js
+++ consultation-site/src/pages/index.js
@@ -11,12 +11,18 @@ import ContentSection from '../components/content-section'
 
 import Skully from '../icons/skully'
 
-const loremIpsum =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus nibh vitae metus luctus condimentum. Ut ut mauris tincidunt, luctus justo nec, commodo nisl. Sed cursus tellus vitae egestas ullamcorper.'
+const loremIpsum = (
+  <div>
+    <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Donec
+    faucibus nibh vitae metus luctus condimentum. <b>Ut ut</b> mauris tincidunt,
+    luctus justo nec, commodo nisl. Sed cursus tellus vitae egestas ullamcorper.
+  </div>
+)
 
 const sections = [
   { name: 'Services', content: loremIpsum },
-  { name: 'About Us', content: loremIpsum },
+  { name: 'Team', content: loremIpsum },
+  { name: 'Dashboard', content: loremIpsum },
   { name: 'Contact Us', content: loremIpsum },
 ]
 
@@ -32,28 +38,38 @@ class IndexPage extends React.Component {
   render() {
     const { scroll } = this.state
 
-    let navigation
+    const Navigation = ({ scroll }) => {
+      if (scroll)
+        return (
+          <nav>
+            {console.log(scroll)}
+            {scroll.children.map((child, i) => (
+              <a
+                key={i}
+                onClick={() => this.scroll.scrollToPosition(child.start)}
+              >
+                <b>{sections[i].name}</b>
+              </a>
+            ))}
+          </nav>
+        )
 
-    if (scroll)
-      navigation = (
-        <nav>
-          {scroll.children.map((child, i) => (
-            <a
-              key={i}
-              onClick={() => this.scroll.scrollToPosition(child.start)}
-            >
-              {sections[i].name}
-            </a>
-          ))}
-        </nav>
-      )
+      return <div />
+    }
 
     return (
       <>
         <Layout>
           <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-          <div style={{ position: 'fixed', width: '100%', height: '100%' }}>
-            {navigation}
+          <div
+            style={{
+              position: 'fixed',
+              left: '0',
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Navigation scroll={scroll} />
             <Scroller
               scrollRef={ref => (this.scroll = ref)}
               autoScroll={true}
@@ -64,7 +80,12 @@ class IndexPage extends React.Component {
                 <section
                   key={index}
                   name={name}
-                  style={{ minHeight: '100vh', display: 'flex' }}
+                  style={{
+                    minHeight: '100vh',
+                    display: 'flex',
+                    background:
+                      'linear-gradient(67.86deg, #101730 -3.34%, #375080 92.35%)',
+                  }}
                 >
                   <LargeMessage color="main" variant="right" icon={Skully}>
                     {content}
