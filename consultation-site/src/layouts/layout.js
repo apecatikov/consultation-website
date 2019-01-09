da84b18a64ff07d041c31b113b6923882e37bc52
apecatikov <antoinepecatikov@gmail.com>
2019-01-08 22:40:45 -0500

    refac(navigation): move navigation to the Layout component

--- consultation-site/src/layouts/layout.js
+++ consultation-site/src/layouts/layout.js
@@ -1,11 +1,67 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { css } from 'emotion'
 
 import './layout.css'
 
+export const loremIpsum = (
+  <div>
+    <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Donec
+    faucibus nibh vitae metus luctus condimentum. <b>Ut ut</b> mauris tincidunt,
+    luctus justo nec, commodo nisl. Sed cursus tellus vitae egestas ullamcorper.
+  </div>
+)
+
+export const sections = [
+  { name: 'Services', content: loremIpsum },
+  { name: 'Team', content: loremIpsum },
+  { name: 'Dashboard', content: loremIpsum },
+  { name: 'Contact Us', content: loremIpsum },
+]
+
+const pageWrapperStyle = css`
+  position: fixed;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(67.86deg, #101730 -3.34%, #375080 92.35%);
+`
+
+const menuItemWrapperStyle = css`
+  display: flex;
+  flexdirection: row;
+  justify-content: center;
+`
+
+const menuItemStyle = css`
+  padding: 0.2rem 0.2rem;
+  color: #f4f4f4;
+`
+
+const Navigation = ({ scroll, onNavClick }) => {
+  if (scroll)
+    return (
+      <nav className={menuItemWrapperStyle}>
+        {scroll.children.map((child, i) => (
+          <a
+            className={menuItemStyle}
+            key={i}
+            onClick={() => onNavClick(child.start)}
+          >
+            <b>{sections[i].name}</b>
+          </a>
+        ))}
+      </nav>
+    )
+
+  return null
+}
+
 class Layout extends React.Component {
   render() {
+    const { scroll, onNavClick } = this.props
+
     return (
       <StaticQuery
         query={graphql`
@@ -20,10 +76,14 @@ class Layout extends React.Component {
           }
         `}
         render={data => (
-          <>
+          <div className={pageWrapperStyle}>
+            <Navigation
+              scroll={scroll}
+              onNavClick={childStartYPos => onNavClick(childStartYPos)}
+            />
             {this.props.children}
             <footer />
-          </>
+          </div>
         )}
       />
     )
--- consultation-site/src/pages/index.js
+++ consultation-site/src/pages/index.js
@@ -2,64 +2,11 @@ import React from 'react'
 import { Scroller, scrollInitialState } from 'react-skroll'
 import { css } from 'emotion'
 
-import Layout from '../layouts/layout'
+import Layout, { loremIpsum, sections } from '../layouts/layout'
 import SEO from '../components/seo'
 import LargeMessage from '../components/large-message'
 
 import Services from '../icons/services'
-
-const loremIpsum = (
-  <div>
-    <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Donec
-    faucibus nibh vitae metus luctus condimentum. <b>Ut ut</b> mauris tincidunt,
-    luctus justo nec, commodo nisl. Sed cursus tellus vitae egestas ullamcorper.
-  </div>
-)
-
-const sections = [
-  { name: 'Services', content: loremIpsum },
-  { name: 'Team', content: loremIpsum },
-  { name: 'Dashboard', content: loremIpsum },
-  { name: 'Contact Us', content: loremIpsum },
-]
-
-const menuItemWrapperStyle = css`
-  display: flex;
-  flexdirection: row;
-  justify-content: center;
-`
-
-const menuItemStyle = css`
-  padding: 0.2rem 0.2rem;
-  color: #f4f4f4;
-`
-
-const pageWrapperStyle = css`
-  position: fixed;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(67.86deg, #101730 -3.34%, #375080 92.35%);
-`
-
-const Navigation = ({ scroll, onNavClick }) => {
-  if (scroll)
-    return (
-      <nav className={menuItemWrapperStyle}>
-        {scroll.children.map((child, i) => (
-          <a
-            className={menuItemStyle}
-            key={i}
-            onClick={() => onNavClick(child.start)}
-          >
-            <b>{sections[i].name}</b>
-          </a>
-        ))}
-      </nav>
-    )
-
-  return null
-}
 
 class IndexPage extends React.Component {
   constructor() {
@@ -74,40 +21,35 @@ class IndexPage extends React.Component {
     const { scroll } = this.state
 
     return (
-      <div>
-        <Layout>
-          <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-          <div className={pageWrapperStyle}>
-            <Navigation
-              scroll={scroll}
-              onNavClick={childStartYPos =>
-                this.scroll.scrollToPosition(childStartYPos)
-              }
-            />
-            <Scroller
-              scrollRef={ref => (this.scroll = ref)}
-              autoScroll={true}
-              autoFrame={true}
-              onScrollChange={scroll => this.setState({ scroll })}
+      <Layout
+        scroll={scroll}
+        onNavClick={childStartYPos =>
+          this.scroll.scrollToPosition(childStartYPos)
+        }
+      >
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Scroller
+          scrollRef={ref => (this.scroll = ref)}
+          autoScroll={true}
+          autoFrame={true}
+          onScrollChange={scroll => this.setState({ scroll })}
+        >
+          {sections.map(({ name, content }, index) => (
+            <section
+              key={index}
+              name={name}
+              style={{
+                minHeight: '100vh',
+                display: 'flex',
+              }}
             >
-              {sections.map(({ name, content }, index) => (
-                <section
-                  key={index}
-                  name={name}
-                  style={{
-                    minHeight: '100vh',
-                    display: 'flex',
-                  }}
-                >
-                  <LargeMessage color="#F4F4F4" variant="right" icon={Services}>
-                    {content}
-                  </LargeMessage>
-                </section>
-              ))}
-            </Scroller>
-          </div>
-        </Layout>
-      </div>
+              <LargeMessage color="#F4F4F4" variant="right" icon={Services}>
+                {content}
+              </LargeMessage>
+            </section>
+          ))}
+        </Scroller>
+      </Layout>
     )
   }
 }
